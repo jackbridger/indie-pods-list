09df162fb6a2d687c5e1ffc523052027e296722c
Jack Bridger <jacksbridger@gmail.com>
2022-03-04 11:45:51 +0000

    list of two indie pods

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	export let name: string;
+	import Pod from "./Pod.svelte";
+	const pods = [{title:'Indie Bites', author:'James McKinven', url:'https://open.spotify.com/show/1MY9XdVViRwfaO8giM4fXO'},{title:'Indie Hackers', author:'Courtland Allen', url:'https://open.spotify.com/show/4ex8hmrHCPvPohKJb3wsuC'}]
+</script>
+
+<main>
+	<h1>Indie Pods List</h1>
+	<ul>
+		{#each pods as pod}
+			<li>
+				<Pod title={pod.title}  author={pod.author} url={pod.url}  />
+			</li>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Pod.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    export let title: string;
+    export let author: string;
+    export let url: string;
+</script>
+
+<main>
+	<h2>{title}</h2>
+    <h3>A pod by {author}</h3>
+	<p>Listen <a href={url}>here</a></p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
